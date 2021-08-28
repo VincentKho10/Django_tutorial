967ebf2627a244e48a83b8218dd2b91027cd9865
vincentkho10 <vincentkho10@gmail.com>
2021-08-28 17:34:14 +0700

    initial push

--- Django_tutorial/urls.py
+++ Django_tutorial/urls.py
@@ -1,4 +1,4 @@
-"""Django_tutor URL Configuration
+"""Django_tutorial URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
